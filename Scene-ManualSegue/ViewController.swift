0563097062520e742db537b95ac6dfca8a0dc0eb
Geon_27 <yukgeon98527@gmail.com>
2022-11-04 02:47:34 +0900

    1104

--- Scene-ManualSegue/ViewController.swift
+++ Scene-ManualSegue/ViewController.swift
@@ -15,5 +15,10 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func wind(_ sender: Any) {
+        
+        // 세그웨이를 실행한다.
+        self.performSegue(withIdentifier:"ManualWind", sender: self)
+    }
 }
 
